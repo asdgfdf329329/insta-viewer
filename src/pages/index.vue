5a898650ff72165bafb80456d4896328de3c91e8
huseeiin <huseeiin@protonmail.com>
2020-09-11 15:05:23 +0300

    feat: initialized search form

--- src/pages/index.vue
+++ src/pages/index.vue
@@ -2,5 +2,18 @@
   <b-container>
     <h1>InstaViewer</h1>
     <p>View Instagram users and posts online</p>
+    <b-form @submit.prevent>
+      <b-form-group>
+        <b-form-input
+          required
+          name="username"
+          autocomplete="off"
+          placeholder="@username"
+        ></b-form-input>
+      </b-form-group>
+      <b-form-group>
+        <b-button variant="primary" type="submit">Search</b-button>
+      </b-form-group>
+    </b-form>
   </b-container>
 </template>
